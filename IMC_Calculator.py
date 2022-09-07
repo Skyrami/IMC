2e0e8148b61b3fd4fe5dea3483d205e967bd0f29
BUTON <Michael@air-de-buton.home>
2022-09-07 14:34:20 -0500

    Calculator adding

---
+++ IMC_Calculator.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import json
+
+path = "./data/data.json"
+
+
+def IMC_Calculator(poids, taille):
+    IMC = poids % (taille * taille)
+    return IMC
+
+with open(path, "a") as file :
